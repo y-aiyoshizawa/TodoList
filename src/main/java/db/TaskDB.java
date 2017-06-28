b808b2cb160834afcad5370d091ffb1316fd7bbd
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-06-28 14:10:35 +0900

    no message

--- src/main/java/db/TaskDB.java
+++ src/main/java/db/TaskDB.java
@@ -1,14 +1,17 @@
 package db;
 
+import java.util.Calendar;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Stateless
 public class TaskDB {
-	@PersistenceContext
+	@PersistenceContext(unitName = "todolist")
 	private EntityManager em;
 	public void create(Task task){
+		task.setDeadline(Calendar.getInstance().getTime());
 		em.persist(task);
 	}
 }
